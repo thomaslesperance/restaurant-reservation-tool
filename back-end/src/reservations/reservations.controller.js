7fb778b615558d9630b7bbdde2ef8aa9000f0dfd
Thomas L'Esperance <144936700+thomaslesperance@users.noreply.github.com>
2024-04-08 15:01:21 -0500

    Completed main dashboard list() route, added error modules.

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,16 @@
+const service = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  // if (req.query) {
+  //   res.json({ data: await service.listReservationsByDate(req.query...) });
+  // }
+  res.json({ data: await service.list() });
 }
 
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
 };
--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -5,8 +5,10 @@
  */
 
 const router = require("express").Router();
-const controller = require("./reservations.controller");
 
-router.route("/").get(controller.list);
+const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
